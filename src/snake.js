75ca863f444f9284e457308f90bc505566d34013
ArunMurugan78 <arunmurugan200@gmail.com>
2020-05-17 23:45:57 +0530

    first commit

---
+++ src/snake.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Box from './box';
+export class SnakeGame extends Component {
+  render() {
+    return (
+    <div>
+<Box/>
+       </div>
+       );
+  }
+}
+
+export default SnakeGame;
